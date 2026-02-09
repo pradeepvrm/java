56387f13c1a454e74df6300ac63f00d9a2a16b6a
Pradeep Verma <pradeepkv012@gmail.com>
2026-02-09 21:17:04 +0530

    Collections framework

---
+++ src/com/pradeep/collections/Main.java
@@ -0,0 +1,24 @@
+package com.pradeep.collections;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        List<Integer> list1 = new LinkedList<>();
+
+        List<Integer> vector = new Vector<>();
+
+        vector.add(15);
+        vector.add(25);
+        vector.add(35);
+
+        System.out.println(vector);
+
+//        list1.add(1);
+//        list1.add(2);
+//        list1.add(3);
+//
+//        System.out.println(list1);
+    }
+}
